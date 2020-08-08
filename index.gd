717b6a03d4d7b770c106282111b1a736803a78bc
Maia Darmon <maia@Maias-MacBook-Air.local>
2020-08-08 00:02:50 -0400

    Change proejcts format

--- index.gd
+++ index.gd
@@ -2,23 +2,12 @@
 
 ## Introduction
 
-<p>Hello! My name is Maia Darmon, and I am entering U3 in Honours Mathematics at McGill University.<br>
+Hello! My name is Maia Darmon, and I am entering U3 in Honours Mathematics at McGill University.<br>
 I am currently exploring different areas of math such as logic, model theory, as well as theoretical computer science, and I am also interested in learning more about group theory, representation theory and Galois theory.
-I am also doing a minor in linguistics, and I would be really excited to learn about fields which combine this with math and computer science.</p>
+I am taking a minor in linguistics as well, and I would be really excited to learn about fields which combine this with math and computer science.</p>
 
 
 ## Projects
 
-<p>In 2019, I did an NSERC Summer Research Project with Pengfei Guan on curve shortening flows and isoperimetric inequalities.<br>
-This summer I am doing an Honours Research Project with Marcin Sabok on Hilbert's <i>Entscheidungsproblem</i> using Gödel's incompleteness theorem and computability theory.</p>
-
-
-<!-- THIS IS A COMMENT -->
-
-<!--
-THIS
-IS
-A
-MULTI-LINE
-COMMENT
--->
+* NSERC Research Project with Pengfei Guan on curve shortening flows and isoperimetric inequalities (Summer 2019)
+* Honours Research Project with Marcin Sabok on Hilbert's <i>Entscheidungsproblem</i> using Gödel's incompleteness theorem and computability theory (Summer 2020)
